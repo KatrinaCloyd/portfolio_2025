d4b72d36003010bb7f721ac1e15adced276e65dd
KatrinaCloyd <katrina.cloyd@yahoo.com>
2025-02-26 15:21:20 -0800

    Add page content and link styling

---
+++ src/components/Icons.tsx
@@ -0,0 +1,34 @@
+export const BURGER = (
+	<>
+        <svg viewBox="0 0 14 2" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 1l12 0" />
+        </svg>
+        <svg viewBox="0 0 14 2" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 1l12 0" />
+        </svg>
+        <svg viewBox="0 0 14 2" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 1l12 0" />
+        </svg>
+    </>
+);
+
+export const CLOSE = (
+	<svg viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1 1l12 12" />
+        <path d="M1 13l12 -12" />
+    </svg>
+);
+
+export const LINK_ARROW = (
+    <svg viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1 7l12 0" />
+        <path d="M6 1l7 6" />
+        <path d="M6 13l7 -6" />
+    </svg>
+);
+
+export const EXTERNAL_LINK = (
+    <svg viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+        <path d="M13 11V1M13 1H3M13 1L2 12"/>
+    </svg>
+);
